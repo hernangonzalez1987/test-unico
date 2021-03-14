3f30fcd6695840a2be997c2283803e4e788070c4
hernangonzales <hernan.gonzales@bexsbanco.com.br>
2021-03-14 16:14:12 -0300

    Adding Initial Load script

--- mysite/initial_load/load.py
+++ mysite/initial_load/load.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 import pandas
 import os
+import sys
 
 DBHOST = os.getenv('DBHOST',default = "localhost")
 DBPORT = os.getenv('DBPORT',default = "5432")
@@ -10,7 +11,12 @@ DBPASS = os.getenv('DBPASS',default = "unico")
 
 engine = create_engine('postgresql://'+ DBUSER +':' + DBPASS + '@' + DBHOST +':' + DBPORT + '/' + DBNAME)
 
-engine.execute('CREATE TABLE IF NOT EXISTS "markets_market" ("id" serial NOT NULL PRIMARY KEY, "longitude" double precision NULL, "latitude" double precision NULL, "setcens" double precision NULL, "areap" double precision NULL, "cod_dist" integer NULL, "district" text NULL, "cod_sub_prefecture" text NULL, "sub_prefecture" text NULL, "region_5" text NULL, "region_8" text NULL, "name" text NULL, "registry" text NULL, "address_street" text NULL, "address_number" text NULL, "address_city" text NULL, "reference" text NULL);')
+result = engine.execute("SELECT EXISTS (SELECT * FROM information_schema.tables WHERE table_name = 'markets_market' );")
+table_exists = result.first()[0]
+
+if not table_exists:
+    print( "Table Markets does not exist. Load should be run only after executing Markets App at least once")
+    sys.exit(1)
 
 market = pandas.read_csv('DEINFO_AB_FEIRASLIVRES_2014.csv')
 market.columns = ['id', 'longitude', 'latitude','setcens','areap','cod_dist','district','cod_sub_prefecture','sub_prefecture','region_5','region_8','name','registry','address_street','address_number','address_city','reference']
